983adb1e3c53f116bb4a9c87915398f0e12d75b6
Marcin Domanski <marcin@domanski.eu>
2018-01-25 00:44:35 +0100

    added Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,46 @@
 import React, {Component} from 'react';
+import {GoMarkGithub} from 'react-icons/lib/go';
+import {FaLinkedinSquare} from 'react-icons/lib/fa';
+import Grid from 'material-ui/Grid';
+import PropTypes from 'prop-types';
+import {withStyles} from 'material-ui/styles';
 
-class Footer
-    extends Component {
+const styles = theme => ({
+    root: {
+
+        width: 30,
+        height: 30,
+        padding: 10,
+    }
+});
+
+class Footer extends Component {
     render() {
-        return (<div>
-                <h1>Footer</h1>
-            </div>
+        const {classes} = this.props;
+        return (
+            <Grid style={{textAlign: "center", paddingTop: 20}}>
+                <span className={classes.root}>Created by Marcin Domański</span>
+                <Grid>
+                    <a href="https://www.linkedin.com/in/m-domanski/">
+                        <FaLinkedinSquare
+                            color="primary"
+                            className={classes.root}
+                        />
+                    </a>
+                    <a href="https://github.com/marcindomanski1/">
+                        <GoMarkGithub
+                            color="primary"
+                            className={classes.root}
+                        />
+                    </a>
+                </Grid>
+            </Grid>
         );
     }
 }
 
-export default Footer;
+Footer.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Footer);
