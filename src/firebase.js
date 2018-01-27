db2bb7f22f03cede556c6f666315921b6ece6869
Marcin Domański <marcin@domanski.eu>
2018-01-27 09:53:57 +0100

    corrected firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyCSzwQhGFsO1MNsNQ8ZlIHlrsuW2L67I8Q",
-  authDomain: "todo-app-9f05c.firebaseapp.com",
-  databaseURL: "https://todoapp-2e2eb.firebaseio.com/",
-  projectId: "todoapp-2e2eb",
-  storageBucket: "todoapp-2e2eb.appspot.com",
-  messagingSenderId: "463760640429"
+    apiKey: "AIzaSyCSzwQhGFsO1MNsNQ8ZlIHlrsuW2L67I8Q",
+    authDomain: "todoapp-2e2eb.firebaseapp.com",
+    databaseURL: "https://todoapp-2e2eb.firebaseio.com",
+    projectId: "todoapp-2e2eb",
+    storageBucket: "todoapp-2e2eb.appspot.com",
+    messagingSenderId: "463760640429"
 };
 
 const firebaseApp = firebase.initializeApp(config);
